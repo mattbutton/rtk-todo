5a2ba200d9f1bd3a94bcb79d35d11888e608bc7c
Matt <matt@mattbutton.com>
2020-01-09 00:32:50 +1300

    Adding todo items

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import AddTodo from 'features/todoList/AddTodo';
 import './App.css';
 
 export default function App() {
   return (
     <div>
-      Hello World
+      <AddTodo />
     </div>
   );
 }
