ca91f53206ed26ce258e2c4d79d40c40684fc119
Matt <matt@mattbutton.com>
2020-05-01 21:14:29 +1200

    Updated createTodoList to match blog post

--- src/features/todoList/todoSlice.ts
+++ src/features/todoList/todoSlice.ts
@@ -30,9 +30,7 @@ const todoSlice = createSlice({
 export const { toggleTodo } = todoSlice.actions;
 
 export const createTodoList = (): AppThunk => async (dispatch: AppDispatch) => {
-  const id = Math.random().toString(36).substr(2, 9); // https://gist.github.com/gordonbrander/2230317
-
-  // Or maybe; https://www.npmjs.com/package/redux-action-router ?
+  const id = Math.random().toString(36).substr(2, 9);
   window.history.pushState(null, document.title, `${id}`);
 }
 
